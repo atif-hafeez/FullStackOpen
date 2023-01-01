8aecfacdc856508b154f9e4509d94968b797e7b3
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-01 23:38:53 +0400

    Event Handling - Step 1

--- part1/state-event/src/App.js
+++ part1/state-event/src/App.js
@@ -3,15 +3,17 @@ import { useState } from "react"
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout (
-    () => setCounter(counter + 1), 
-    1000
-  );
+  const handleClick = () => {
+    console.log('clicked');
+  }
 
-  console.log('rendering...', counter);
-  
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus
+      </button>
+    </div>
   )
 }
 
