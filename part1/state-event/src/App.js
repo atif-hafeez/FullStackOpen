02f94e7d06f7df52dfa4f9ff5b3ba4a1e9a542de
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-02 19:17:02 +0400

    Refactoring the components - Buttons Component

--- part1/state-event/src/App.js
+++ part1/state-event/src/App.js
@@ -2,14 +2,7 @@ import { useState } from "react"
 
 const Display = ({counter}) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [counter, setCounter] = useState(0);
