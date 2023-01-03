ab3fa53995384d7050ced29c851955bcb6c133e8
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-04 00:14:09 +0400

    unicafe step2

--- part1/submission/unicafe/src/App.js
+++ part1/submission/unicafe/src/App.js
@@ -1,14 +1,54 @@
 import { useState } from 'react'
 
 const App = () => {
+  
+  /* const weight = {
+    good: 1,
+    neutral: 0, 
+    bad: -1
+  } */
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allStats, setAllStats] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  
+  const handleGoodClick = () => {
+    setGood(good+1);
+    setAllStats(allStats + 1);
+    
+    const newGood = good + 1;
+    const newAllStats = allStats + 1
+    
+    setAverage((newGood * 1 + neutral * 0 + bad * -1)/newAllStats)
 
-  const handleGoodClick = () => setGood(good+1);
-  const handleNeutralClick = () => setNeutral(neutral+1);
-  const handleBadClick = () => setBad(bad+1);
+    setPositive(newGood/newAllStats)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1);
+    setAllStats(allStats+1);
+    
+    const newNeutral = neutral + 1;
+    const newAllStats = allStats + 1
+    
+    setAverage((good * 1 + newNeutral * 0 + bad * -1)/newAllStats)
+
+    setPositive(good/newAllStats)
+  }
+  const handleBadClick = () => {
+    setBad(bad+1);
+    setAllStats(allStats+1);
+    
+    const newBad = bad + 1;
+    const newAllStats = allStats + 1
+    
+    setAverage((good * 1 + neutral * 0 + newBad * -1)/newAllStats)
+
+    setPositive(good/newAllStats)
+  }
 
   return (
     <div>
@@ -25,6 +65,12 @@ const App = () => {
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        all {allStats}
+        <br />
+        average {average}
+        <br />
+        positive {positive*100} %
       </p>
     </div>
   )
