120bf71cee715ee47d95e7af6618a88cfd0b36f6
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-04 13:29:43 +0400

    rendering completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -33,22 +33,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
-      parts:[
+    parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
-        name:'Using props to pass data',
-        exercises: 7
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
       },
       {
-        name:'State of a component',
-        exercises: 14
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
       }
     ]
-  };
+  }
 
   return (
     <div>
