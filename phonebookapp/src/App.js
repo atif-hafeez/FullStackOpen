c5cbb41452ef6f88808f07011c2a3632a9832954
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-06 23:12:47 +0400

    The phonebook Step1

--- phonebookapp/src/App.js
+++ phonebookapp/src/App.js
@@ -6,19 +6,39 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleAdd = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+      <div>
+        <h2>Phonebook</h2>
+        <form>
+          <div>
+            name: <input value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <button  onClick={handleAdd}>add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        {persons.map(person => <><span key={person.name}>{person.name}</span><br /></>)}
+      </div>
+      <div>
+        <p>debug: {newName}</p>
+      </div>
     </div>
   )
 }
