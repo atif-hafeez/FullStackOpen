388a966563b86293311d6f6ab0f6eff55bd8c9bb
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-07 11:32:57 +0400

    Install Axios,get intial data using axios in index

--- phonebookapp/src/App.js
+++ phonebookapp/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {  useState } from 'react'
 
 const Filter = ({searchName, onSearchChange}) => {
   return (
@@ -38,14 +38,9 @@ const Persons = ({persons}) => {
 }
 
 
-const App = () => {
+const App = (props) => {
   //Define Phonebook Object
-  const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456'},
-      { name: 'Ada Lovelace', number: '39-44-5323523'},
-      { name: 'Dan Abramov', number: '12-43-234345'},
-      { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState(props.persons) 
 
   //Define state variables to Control form input elements
   const [newName, setNewName] = useState('')
--- phonebookapp/src/index.js
+++ phonebookapp/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-
 import App from './App'
+import axios from 'axios'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const promise = axios.get('http://localhost:3001/persons').then((response) => {
+  const persons = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App persons={persons} />
+  );
+
+})
+
+
+
 
 
