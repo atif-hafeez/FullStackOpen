0fa34426b717f8d2fc200a0db9512135e5531715
Atif Hafeez <atif.hafeez@transcore.com>
2023-01-07 11:45:30 +0400

    Ex: 2. 11 - Thephonebook Step6

--- phonebookapp/src/index.js
+++ phonebookapp/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
-import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/persons').then((response) => {
-  const persons = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App persons={persons} />
-  );
 
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
 
 
 
